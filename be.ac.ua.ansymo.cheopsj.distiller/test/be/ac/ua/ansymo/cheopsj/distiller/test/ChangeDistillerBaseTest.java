6c51128a1bd4d8edf19fc594b192b006aee3ce49
Pieter De Maeyer <Pieter.DeMaeyer@student.ua.ac.be>
2013-06-05 20:09:50 +0200

    Moved tests again to distiller package, some tests must be run inside an eclipse instance. An external library crashes if this is not the case

---
+++ be.ac.ua.ansymo.cheopsj.distiller/test/be/ac/ua/ansymo/cheopsj/distiller/test/ChangeDistillerBaseTest.java
@@ -0,0 +1,241 @@
+package be.ac.ua.ansymo.cheopsj.distiller.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.*;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.print.DocFlavor.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+
+
+import org.tmatesoft.svn.core.SVNDepth;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNProperties;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
+import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
+import org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory;
+import org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import org.tmatesoft.svn.core.wc.*;
+import org.tmatesoft.svn.core.wc.admin.SVNAdminClient;
+import org.apache.commons.io.FileUtils;
+
+//import be.ac.ua.ansymo.cheopsj.distiller.popup.actions.ChangeExtractor;
+import be.ac.ua.ansymo.cheopsj.distiller.popup.actions.DistillChanges;
+
+public class ChangeDistillerBaseTest extends TestCase{
+	protected static String REPO_PATH = "/tmp/svn-test-repo";
+	protected static String REPO_LOCAL_PATH = "/tmp/svn-test-local";
+	private static String RES_DIR = null;
+
+	public void setUp() {
+		// Init SVN lib
+		SVNRepositoryFactoryImpl.setup(); // for svn and svn+ssh protocols
+		DAVRepositoryFactory.setup(); // for http(s) protocol
+		FSRepositoryFactory.setup(); // for local access (file protocol).
+
+		// Creating temporary repository.
+		File repo_dir = new File(REPO_PATH);
+		SVNURL tgtURL = null;
+		try {
+			tgtURL = SVNRepositoryFactory.createLocalRepository(repo_dir, true,
+					true);
+
+		} catch (SVNException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		// create a local working copy.
+		SVNClientManager clientManager = SVNClientManager.newInstance();
+		
+		SVNUpdateClient updateClient = clientManager.getUpdateClient();
+		File local_dir = new File(REPO_LOCAL_PATH);
+		// local_dir.deleteOnExit();
+		try {
+			updateClient.doCheckout(SVNURL.fromFile(repo_dir), local_dir,
+					SVNRevision.UNDEFINED, SVNRevision.HEAD, SVNDepth.INFINITY,
+					false);
+		} catch (SVNException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		ChangeExtractor ce = new ChangeExtractor();
+	}
+
+	public void tearDown() {
+		ChangeExtractor.resetCounters();
+		File repo_dir = new File(REPO_PATH);
+		File local_dir = new File(REPO_LOCAL_PATH);
+		// delete repository and local working copy.
+		try {
+			FileUtils.deleteDirectory(local_dir);
+			FileUtils.deleteDirectory(repo_dir);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("=============");
+	}
+
+	public void runDistiller() {
+
+		// Modifying distillchanges
+		DistillChanges distiller = new DistillChanges();
+
+		// Bypass all encapsulation, accessing private methods and fields
+		// directly.
+		Class c = distiller.getClass();
+		try {
+			// Replace private selectedProject with a stub.
+			Field project = c.getDeclaredField("selectedProject");
+			project.setAccessible(true);
+			project.set(distiller, new EclipseProjectStub(REPO_LOCAL_PATH));
+
+			// Launch private method iterateRevisions.
+			Method iterate = c.getDeclaredMethod("iterateRevisions",
+					IProgressMonitor.class);
+			iterate.setAccessible(true);
+			iterate.invoke(distiller, new EclipseProgressMonitorStub());
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * Set the directory to use for commits in the tests resource directory.
+	 */
+	public void setResourceDir(String path){
+		RES_DIR = path;
+	}
+	
+	/*
+	 * Do the commits contained in the subdirectories of the resource directory.
+	 * All files in directory named 1 will be copied to the local working directory and committed.
+	 * Then all files in directory named 2 will be copied to the local working directory and committed.
+	 * And so on. If a file is not present in the next commit, it will be deleted from the local working copy.
+	 * 
+	 * NOTE: subdirectories are not supported, only files.
+	 */
+	@SuppressWarnings("deprecation")
+	public void doCommit(){
+		SVNRepositoryFactoryImpl.setup(); 
+		FSRepositoryFactory.setup(); 
+		DAVRepositoryFactory.setup(); 
+		
+		//String file = this.getClass().getResource(RES_DIR).getFile();
+		Bundle bundle = Platform.getBundle("be.ac.ua.ansymo.cheopsj.distiller");
+		java.net.URL fileURL = bundle.getEntry("resources/"+RES_DIR);
+		File local_dir = null;
+		String file = fileURL.toString();
+		try {
+			local_dir = new File(FileLocator.resolve(fileURL).toURI());
+		} catch (URISyntaxException e1) {
+		    e1.printStackTrace();
+		} catch (IOException e1) {
+		    e1.printStackTrace();
+		}
+		
+		//File local_dir = new File(REPO_LOCAL_PATH);
+		SVNClientManager clientManager = SVNClientManager.newInstance();
+		SVNCommitClient commitClient = clientManager.getCommitClient();
+		SVNStatusClient statusClient = clientManager.getStatusClient(); 
+		SVNWCClient wcClient = clientManager.getWCClient(); 
+		SVNUpdateClient updateClient = clientManager.getUpdateClient();
+		
+		int i = 1;
+		File next_commit = new File(file+"/"+1);
+		while(next_commit.exists() && next_commit.isDirectory()){
+			//Delete files that are not present in the directory anymore.
+			String[] commit_file_list = next_commit.list();
+			String[] local_file_list = local_dir.list();
+			ArrayList<String> commit_list = new ArrayList<String>(Arrays.asList(commit_file_list));
+			ArrayList<String> local_list = new ArrayList<String>(Arrays.asList(local_file_list));
+			local_list.removeAll(commit_list);
+			
+			for(String file_del : local_list){
+				if(!file_del.equals(".svn")){
+					//Delete file from local repo.
+					File del = new File(REPO_LOCAL_PATH+"/"+file_del);
+					System.out.println(del.toString());
+					
+					try {
+						SVNURL[] svnpath = {SVNURL.fromFile(del)};
+						wcClient.doDelete(del, false, true, false);
+					} catch (SVNException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			//Adding new files.
+			local_list = new ArrayList<String>(Arrays.asList(local_file_list));
+			commit_list = new ArrayList<String>(Arrays.asList(commit_file_list));
+			commit_list.removeAll(local_list);
+			try {
+				FileUtils.copyDirectory(next_commit, local_dir);
+				SVNProperties properties = new SVNProperties();
+				for(String commit : commit_list){
+					if(!commit.equals(".svn")){
+						File file_add = new File(REPO_LOCAL_PATH+"/"+commit);
+						try {
+							wcClient.doAdd(file_add, false, false, false, false, false);
+						} catch (SVNException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			//Do commit and update.
+			File[] temp = {local_dir};
+			try {
+				commitClient.doCommit(temp, true, "commit", null, null, false, false, SVNDepth.INFINITY);
+				updateClient.doUpdate(local_dir, SVNRevision.HEAD, SVNDepth.INFINITY, false, false);
+			} catch (SVNException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			i++;
+			next_commit = new File(file+"/"+i);
+		}
+		//Commit changes
+	}
+}
---
+++ be.ac.ua.ansymo.cheopsj.distiller/test/be/ac/ua/ansymo/cheopsj/distiller/test/EclipseProgressMonitorStub.java
@@ -0,0 +1,54 @@
+package be.ac.ua.ansymo.cheopsj.distiller.test;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class EclipseProgressMonitorStub implements IProgressMonitor{
+
+	@Override
+	public void beginTask(String name, int totalWork) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void done() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void internalWorked(double work) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isCanceled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setCanceled(boolean value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTaskName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void subTask(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void worked(int work) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
